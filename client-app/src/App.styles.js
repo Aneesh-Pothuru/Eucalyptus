cfb61723ea808ef9ae13d7adda5e80b1021ad98e
Aneesh Pothuru <pothuruaneesh23@gmail.com>
2021-03-27 18:29:02 -0700

    fixed styled and adjusted views

--- client-app/src/App.styles.js
+++ client-app/src/App.styles.js
@@ -2,8 +2,10 @@ import styled from 'styled-components';
 
 export const AppContainer = styled.div`
   text-align: center;
+  color: white;
 `;
 
 export const ImageContainer = styled.img`
+  height: 30vmin;
   padding: 15px;
 `
